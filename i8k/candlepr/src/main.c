13cb8c1c18505c2610ddb73e0cc807e13bbadfb3
vbushuev <v.bushuev@gmail.com>
2016-02-18 01:31:36 +0300

    try scenario

--- i8k/candlepr/src/main.c
+++ i8k/candlepr/src/main.c
@@ -10,7 +10,7 @@ void main(void){
 	int iTime,counter=0,ioLength,io;
 	// scenario control
 	int sstep=0, currentScenario = 0;
-	//unsigned long wait_di = 0;
+	unsigned long wait_di = 0;
 	// DI_DO modules reading buffer
 	unsigned long di_data=0,do_data=0;
 	long encLUp,encLDown,encRUp,encRDown;
@@ -60,7 +60,7 @@ void main(void){
 		 switch(currentScenario){
 			 case 0:
 			 	do_data = initScenario[sstep].command;
-				//wait_di = initScenario[sstep].wait;
+				wait_di = initScenario[sstep].wait.value;
 
 		 }
 		 /**************************************************************************
